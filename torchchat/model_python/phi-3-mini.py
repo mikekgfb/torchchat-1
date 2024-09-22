56826648ea61ad191377843c226979085c9cb8cc
Mike G <mkgschwind+private@gmail.com>
2024-09-22 16:32:35 -0700

    Update phi-3-mini.py
    ModelArgs

--- torchchat/model_python/phi-3-mini.py
+++ torchchat/model_python/phi-3-mini.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 
 from transformers import AutoModelForCausalLM # , AutoTokenizer, pipeline
-from torchchat.model import TransformerArgs
+from torchchat.model import ModelArgs
 
 def model_builder(builder_args) -> nn.Module:
     torch.random.manual_seed(0)
@@ -15,8 +15,11 @@ def model_builder(builder_args) -> nn.Module:
     )
 
     # let's get a default config SentencePiece
-    # PS: SentencePiece is the default, but we assert it in the constructor for documentation
-    model.config = TransformerArgs(use_tiktoken=False)
+    # PS: Mostly default values, but we assert it in the constructor for documentation
+    model.config = ModelArgs(
+        transformer_args=None,
+        model_type=ModelType.TextOnly,
+        use_tiktoken=False)
     print(model)
 
     return model
