1cd7699e3512264710199cfa5e0fcc01e53fa74c
Mike G <mkgschwind+private@gmail.com>
2024-09-23 08:35:30 -0700

    Update phi-3-mini.py
    Add setup caches function

--- torchchat/model_python/phi-3-mini.py
+++ torchchat/model_python/phi-3-mini.py
@@ -1,6 +1,7 @@
 
 import torch
 import torch.nn as nn
+import types
 
 from transformers import AutoModelForCausalLM # , AutoTokenizer, pipeline
 from torchchat.model import ModelArgs, ModelType, TextOnlyModel, TransformerArgs
@@ -21,6 +22,12 @@ def model_builder(builder_args) -> nn.Module:
         model_type=ModelType.TextOnly,
         use_tiktoken=False)
     model.config = TransformerArgs()
+    import types
+
+    def setup_caches(self):
+        print(f"setup caches for {self}, no-op")
+
+    model.setup_caches = types.MethodType(setup_caches, model)
     print(model)
 
     model = TextOnlyModel(model_config, {"text" : model})
